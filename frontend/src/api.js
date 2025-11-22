5429850005d12fe8e73249e616b77709d3a62c02
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-11-22 22:43:36 +0530

    Updated the  backend link

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://crudformompharmacy.onrender.com/api",
 });
 
 export const getProjects = () => API.get("/projects");
